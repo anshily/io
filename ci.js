6bc3e50650b43244da82132efa3bde4da684d547
anshily <45704869+anshily@users.noreply.github.com>
2020-03-10 17:36:52 +0800

    no message

---
+++ ci.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var process = require('child_process');
+
+http.createServer(function (request, response) {
+
+    process.exec('git pull',function (error, stdout, stderr) {
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+    });
+    // 发送 HTTP 头部 
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}).listen(8888);
